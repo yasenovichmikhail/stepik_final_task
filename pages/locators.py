de4c6fd793117648e81d34b12771ea2cdc5d8cd2
user <yasenovich.mikhail@mail.ru>
2023-07-26 17:21:11 +0300

    created login page module. Modified locators and main_page

--- pages/locators.py
+++ pages/locators.py
@@ -1,5 +1,9 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
+class MainPageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators:
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
