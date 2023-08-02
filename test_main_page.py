c4ba097bea9d44b1ba319002fc790031dcbf64c5
user <yasenovich.mikhail@mail.ru>
2023-08-02 13:37:10 +0300

    added test_guest_cant_see_product_in_basket_opened_from_product_page and test_guest_cant_see_product_in_basket_opened_from_main_page test cases. Added should_not_be_basket_item and should_be_text_empty_basket methods to basket_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 import pytest
-from selenium.webdriver.common.by import By
+
+from .pages.basket_page import BasketPage
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 @pytest.mark.skip
@@ -19,7 +20,7 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
-
+@pytest.mark.skip
 def test_guest_should_see_login_form(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = LoginPage(browser, link)
@@ -27,7 +28,7 @@ def test_guest_should_see_login_form(browser):
     page.go_to_login_page()
     page.should_be_login_form()
 
-
+@pytest.mark.skip
 def test_guest_should_see_register_form(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = LoginPage(browser, link)
@@ -35,10 +36,19 @@ def test_guest_should_see_register_form(browser):
     page.go_to_login_page()
     page.should_be_register_form()
 
-
+@pytest.mark.skip
 def test_current_url_should_contain_login(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = LoginPage(browser, link)
     page.open()
     page.go_to_login_page()
     page.should_be_login_url()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    page.should_not_be_basket_item()
+    page.should_be_text_empty_basket()
--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 import pytest
+from .pages.basket_page import BasketPage
 from .pages.product_page import ProductPage
 import time
 
@@ -15,7 +16,7 @@ def test_guest_can_add_product_to_basket(browser, promo_offer):
     page.solve_quiz_and_get_code()
     page.check_name_added_book()
     page.check_price_added_book()
-    # time.sleep(1000)
+    time.sleep(1000)
 
 
 @pytest.mark.skip
@@ -41,17 +42,25 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.add_product_to_basket()
     page.is_disappeared_success_message()
 
-
+@pytest.mark.skip
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
 
-
+@pytest.mark.skip
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    page.should_not_be_basket_item()
+    page.should_be_text_empty_basket()
